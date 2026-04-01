9317f40468da669d2b4b8b2e43791c268cf56f60
Miguel <miganjos88@gmail.com>
2026-04-01 18:43:57 -0300

    feat: initial commit

---
+++ backend/src/main/java/com/fiap/hackgov/HackgovApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackgov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackgovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackgovApplication.class, args);
+	}
+
+}
