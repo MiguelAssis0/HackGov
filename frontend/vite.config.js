cdde0e9949af793f73a098a7ae94de0feef4ca10
Miguel <miganjos88@gmail.com>
2026-06-04 22:51:37 -0300

    feat: add docker config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://hackgov-backend:8080",
         changeOrigin: true,
-      }
+      },
     },
   },
 });
